1deea27b8ef859485133806c96fe31502202c4fe
nick786-cybe <65865069+nick786-cybe@users.noreply.github.com>
2020-09-30 08:10:32 -0700

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -88,7 +88,10 @@ if __name__ == '__main__':
 
         elif 'open google' in query:
             webbrowser.get('chrome').open_new_tab('https://google.com')
-
+        elif 'search' in query:
+            query = query.replace('search', '')
+            webbrowser.get('chrome').open_new_tab('https://www.google.com/search?q=(query)')
+            
         elif 'open stackoverflow' in query:
             webbrowser.get('chrome').open_new_tab('https://stackoverflow.com')
 
